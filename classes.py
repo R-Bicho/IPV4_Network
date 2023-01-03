8be65650b9edccc1777a0053d8704f38d9d7f81d
R-Bicho <rogerio-bicho@outlook.com>
2023-01-03 12:05:05 -0300

    created the main funcs for the project

--- classes.py
+++ classes.py
@@ -15,17 +15,71 @@ class CalculateIPV4:
 
         if self.validate_ip():
             pass
+        else:
+            raise ValueError('You need write only IP')
+
+        self.network_mask()
 
     def validate_ip(self):
+        '''checked if ip is correct'''
+
         ip_regexp = re.compile(
             r'^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$')
 
         if ip_regexp.search(self.ip):
-            print('tem')
             return True
-        else:
-            print('Não tem')
-            return False
+        return False
+
+    def separete_ip(self):
+        '''ip section of prefix'''
+
+        value = self.ip.split('/')
+        only_ip = value[0]
+        self.prefix = value[1]
+        return only_ip, self.prefix
+
+    def ip_for_binary(self):
+        '''Converted IP for binary value'''
+
+        only_ip, _ = self.separete_ip()
+        remove_special_caracters = only_ip.split('.')
+        new_ip = int(''.join(remove_special_caracters))
+        binary_ip = bin(new_ip)
+        return binary_ip[2:]
+
+    def total_hosts(self):
+        '''Calculated the total number of valid hosts on the network'''
+
+        _, prefix = self.separete_ip()
+        temporary = 32 - int(prefix)
+        hosts = (2**temporary) - 2
+        return hosts
+
+    def network_mask(self):
+        '''Calculated the sub-network mask'''
+
+        _, prefix = self.separete_ip()
+        bits_value_one = '1' * int(prefix)
+        bits_value_zero = '0' * (32 - int(prefix))
+        total_bits = bits_value_one + bits_value_zero
+
+        separate_bits = []
+        group_bits = []
+
+        for value in total_bits:
+            separate_bits.append(value)
+
+        group_bits.append(''.join(separate_bits[0:8]))
+        group_bits.append(''.join(separate_bits[8:16]))
+        group_bits.append(''.join(separate_bits[16:24]))
+        group_bits.append(''.join(separate_bits[24:]))
+
+        temporary_mask = []
+        for i in group_bits:
+            temporary_mask.append(str(int(i, 2)))
+
+        mask = '.'.join(temporary_mask)
+        return mask
 
 
 if __name__ == '__main__':
